d24788b9b8a864bda11bcc77b58bbcda633815f5
zhengw060024 <zhengwei060024@hotmail.com>
2017-11-13 22:57:51 +0800

    xiugai

--- algorithemJs/chapter1/chapter1program.js
+++ algorithemJs/chapter1/chapter1program.js
@@ -1,12 +1,70 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * 算法导论第一章的算法和习题js实现练习
  * 2017.11.11
  */
-function generateRandomArray(min, max, itemNum) {
+var utilitytools_1 = require("./utilitytools");
+/**
+ * 排序校验 不使用标准排序方法
+ * @param ArrayInput
+ * @param ArrayOutput
+ */
+function checkSequence(ArrayInput, ArrayOutput) {
+    var objTemp = {};
+    ArrayInput.forEach(function (value, indxe) {
+        if (objTemp[value]) {
+            ++objTemp[value];
+        }
+        else {
+            objTemp[value] = 1;
+        }
+    });
+    for (var i = 0; i < ArrayOutput.length - 1; ++i) {
+        if (objTemp[ArrayOutput[i]]) {
+            --objTemp[ArrayOutput[i]];
+        }
+        else {
+            return false;
+        }
+    }
+    for (var key in objTemp) {
+        if (objTemp[key] !== 0) {
+            return false;
+        }
+    }
+    // 检查是否从小到大
+    for (var i = 0; i < ArrayOutput.length - 1; ++i) {
+        if (ArrayOutput[i] > ArrayOutput[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 /**
- * 插入排序
+ * 排序检测，使用标准排序准则
+ * @param ArrayInput
+ * @param ArrayOutput
+ */
+function checkSeqUseStdSort(ArrayInput, ArrayOutput) {
+    if (ArrayInput.length !== ArrayOutput.length) {
+        return false;
+    }
+    var ArrayToSort = ArrayInput.concat([]);
+    ArrayToSort.sort();
+    for (var i = 0; i < ArrayToSort.length; ++i) {
+        if (ArrayToSort[i] !== ArrayOutput[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+///////////////////////////////////////////////////////////////////
+////////////插入排序
+///////////////////////////////////////////////////////////////////
+/**
+ * 插入排序非递归
+ * @param ArrayInput
  */
 function insertSort(ArrayInput) {
     for (var i = 1; i < ArrayInput.length; ++i) {
@@ -24,6 +82,38 @@ function insertSort(ArrayInput) {
     }
     return ArrayInput;
 }
+/**
+ * 插入排序递归
+ * @param ArrayInput
+ * @param nNumToSort 要排序的的最后一个关键字的index
+ */
+function insertSortRecursion(ArrayInput, nNumToSort) {
+    if (nNumToSort === 0) {
+        return;
+    }
+    insertSortRecursion(ArrayInput, nNumToSort - 1);
+    var keyItem = ArrayInput[nNumToSort];
+    var i = nNumToSort - 1;
+    for (; i >= 0; --i) {
+        if (ArrayInput[i] > keyItem) {
+            ArrayInput[i + 1] = ArrayInput[i];
+        }
+        else {
+            break;
+        }
+    }
+    ArrayInput[i + 1] = keyItem;
+}
+//////////////////////////////////////////////////////////
+////////////归并排序
+//////////////////////////////////////////////////////////
+/**
+ * 归并排序归并过程
+ * @param ArrayInput
+ * @param start
+ * @param middle
+ * @param end
+ */
 function merge(ArrayInput, start, middle, end) {
     var ArrayFirst = ArrayInput.slice(start, middle + 1);
     var ArraySecond = ArrayInput.slice(middle + 1, end + 1);
@@ -51,6 +141,12 @@ function merge(ArrayInput, start, middle, end) {
         }
     }
 }
+/**
+ * 归并排序
+ * @param ArrayInput
+ * @param start
+ * @param end
+ */
 function mergeSort(ArrayInput, start, end) {
     if (start < end) {
         var middle = Math.floor((start + end) / 2);
@@ -60,9 +156,78 @@ function mergeSort(ArrayInput, start, end) {
     }
     return ArrayInput;
 }
+////////////////////////////////////////////////////////
+//////// 二分查找法 
+////////////////////////////////////////////////////////
+/**
+ * 二分查找法迭代
+ * @param ArraySorted
+ * @param NumToSearch
+ */
+function binarySearchIteration(ArraySorted, NumToSearch) {
+    var nStart = 0;
+    var nEnd = ArraySorted.length - 1;
+    while (nStart < nEnd) {
+        var nMiddle = Math.floor((nEnd + nStart) / 2);
+        if (ArraySorted[nMiddle] === NumToSearch) {
+            return nMiddle;
+        }
+        if (ArraySorted[nMiddle] < NumToSearch) {
+            nStart = nMiddle + 1;
+        }
+        else {
+            nEnd = nMiddle - 1;
+        }
+    }
+    if (nStart === nEnd) {
+        if (ArraySorted[nStart] === NumToSearch) {
+            return nStart;
+        }
+    }
+    return -1;
+}
+function binarySearchRecursion(ArraySorted, NumberSearch, nStartIndex, nEndIndex) {
+    if (nStartIndex < nEndIndex) {
+        var nMiddle = Math.floor((nEndIndex + nStartIndex) / 2);
+        if (ArraySorted[nMiddle] === NumberSearch) {
+            return nMiddle;
+        }
+        if (ArraySorted[nMiddle] > NumberSearch) {
+            return binarySearchRecursion(ArraySorted, NumberSearch, nStartIndex, nMiddle - 1);
+        }
+        else {
+            return binarySearchRecursion(ArraySorted, NumberSearch, nMiddle + 1, nEndIndex);
+        }
+    }
+    else if (nStartIndex === nEndIndex) {
+        if (NumberSearch === ArraySorted[nStartIndex]) {
+            return nStartIndex;
+        }
+        return -1;
+    }
+    else {
+        return -1;
+    }
+}
+/////
+/////测试----------
 console.log(insertSort([3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8]));
 var ArrayMerge = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+var ArrayMerge2 = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+ArrayMerge2.sort(function (a, b) {
+    return a - b;
+});
+console.log("standard sort " + ArrayMerge2);
+var ArrayMerge3 = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+insertSortRecursion(ArrayMerge3, ArrayMerge3.length - 1);
+console.log("insert sort " + ArrayMerge3);
 mergeSort(ArrayMerge, 0, ArrayMerge.length - 1);
 console.log(ArrayMerge);
+console.log('binarySearchIteration test', binarySearchIteration(ArrayMerge, 12));
+console.log('binarySearchRecursion test', binarySearchRecursion(ArrayMerge, 12, 0, ArrayMerge.length - 1));
 function testCaseInsertSort() {
+    var ArrayTest = utilitytools_1.default.generateRandomArray(1, 100, 20);
+    console.log("orgin sequence is " + ArrayTest);
+    insertSort(ArrayTest);
+    console.log("sequence sorted is " + ArrayTest);
 }
--- algorithemJs/chapter1/chapter1program.ts
+++ algorithemJs/chapter1/chapter1program.ts
@@ -3,18 +3,64 @@
  * 2017.11.11
  */
 import utilityTools from './utilitytools'
-
-function checkSequence(ArrayInput: Array<number>) {
-    for (let i = 0; i < ArrayInput.length - 1; ++i) {
-        if (ArrayInput[i] > ArrayInput[i + 1]) {
+/**
+ * 排序校验 不使用标准排序方法
+ * @param ArrayInput 
+ * @param ArrayOutput 
+ */
+function checkSequence(ArrayInput: Array<number>, ArrayOutput: Array<number>) {
+    const objTemp: any = {};
+    ArrayInput.forEach((value, indxe) => {
+        if (objTemp[value]) {
+            ++objTemp[value];
+        } else {
+            objTemp[value] = 1;
+        }
+    });
+    for (let i = 0; i < ArrayOutput.length - 1; ++i) {
+        if (objTemp[ArrayOutput[i]]) {
+            --objTemp[ArrayOutput[i]];
+        } else {
+            return false;
+        }
+    }
+    for (let key in objTemp) {
+        if (objTemp[key] !== 0) {
+            return false;
+        }
+    }
+    // 检查是否从小到大
+    for (let i = 0; i < ArrayOutput.length - 1; ++i) {
+        if (ArrayOutput[i] > ArrayOutput[i + 1]) {
             return false;
         }
     }
     return true;
 }
-
 /**
- * 插入排序
+ * 排序检测，使用标准排序准则
+ * @param ArrayInput 
+ * @param ArrayOutput 
+ */
+function checkSeqUseStdSort(ArrayInput: Array<number>, ArrayOutput: Array<number>) {
+    if (ArrayInput.length !== ArrayOutput.length) {
+        return false;
+    }
+    const ArrayToSort = ArrayInput.concat([]);
+    ArrayToSort.sort();
+    for (let i = 0; i < ArrayToSort.length; ++i) {
+        if (ArrayToSort[i] !== ArrayOutput[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+///////////////////////////////////////////////////////////////////
+////////////插入排序
+///////////////////////////////////////////////////////////////////
+/**
+ * 插入排序非递归
+ * @param ArrayInput 
  */
 function insertSort(ArrayInput: Array<number>) {
 
@@ -32,6 +78,37 @@ function insertSort(ArrayInput: Array<number>) {
     }
     return ArrayInput;
 }
+/**
+ * 插入排序递归
+ * @param ArrayInput 
+ * @param nNumToSort 要排序的的最后一个关键字的index
+ */
+function insertSortRecursion(ArrayInput: Array<number>, nNumToSort: number) {
+    if (nNumToSort === 0) {
+        return;
+    }
+    insertSortRecursion(ArrayInput, nNumToSort - 1);
+    const keyItem = ArrayInput[nNumToSort];
+    let i = nNumToSort - 1
+    for (; i >= 0; --i) {
+        if (ArrayInput[i] > keyItem) {
+            ArrayInput[i + 1] = ArrayInput[i];
+        } else {
+            break;
+        }
+    }
+    ArrayInput[i + 1] = keyItem;
+}
+//////////////////////////////////////////////////////////
+////////////归并排序
+//////////////////////////////////////////////////////////
+/**
+ * 归并排序归并过程
+ * @param ArrayInput 
+ * @param start 
+ * @param middle 
+ * @param end 
+ */
 function merge(ArrayInput: Array<number>, start: number, middle: number, end: number) {
     const ArrayFirst = ArrayInput.slice(start, middle + 1);
     const ArraySecond = ArrayInput.slice(middle + 1, end + 1);
@@ -58,6 +135,12 @@ function merge(ArrayInput: Array<number>, start: number, middle: number, end: nu
         }
     }
 }
+/**
+ * 归并排序
+ * @param ArrayInput 
+ * @param start 
+ * @param end 
+ */
 function mergeSort(ArrayInput: Array<number>, start: number, end: number) {
     if (start < end) {
         const middle = Math.floor((start + end) / 2);
@@ -67,13 +150,76 @@ function mergeSort(ArrayInput: Array<number>, start: number, end: number) {
     }
     return ArrayInput;
 }
+////////////////////////////////////////////////////////
+//////// 二分查找法 
+////////////////////////////////////////////////////////
+/**
+ * 二分查找法迭代
+ * @param ArraySorted 
+ * @param NumToSearch 
+ */
+function binarySearchIteration(ArraySorted: Array<number>, NumToSearch: number) {
+    let nStart = 0; let nEnd = ArraySorted.length - 1;
+    while (nStart < nEnd) {
+        let nMiddle = Math.floor((nEnd + nStart) / 2)
+        if (ArraySorted[nMiddle] === NumToSearch) {
+            return nMiddle;
+        }
+        if (ArraySorted[nMiddle] < NumToSearch) {
+            nStart = nMiddle + 1;
+        } else {
+            nEnd = nMiddle - 1;
+        }
+    }
+    if (nStart === nEnd) {
+        if (ArraySorted[nStart] === NumToSearch) {
+            return nStart;
+        }
+    }
+    return - 1;
+}
+function binarySearchRecursion(ArraySorted: Array<number>, NumberSearch: number,
+    nStartIndex: number, nEndIndex: number): number {
+    if (nStartIndex < nEndIndex) {
+        const nMiddle: number = Math.floor((nEndIndex + nStartIndex) / 2);
+        if (ArraySorted[nMiddle] === NumberSearch) {
+            return nMiddle;
+        }
+        if (ArraySorted[nMiddle] > NumberSearch) {
+            return binarySearchRecursion(ArraySorted, NumberSearch, nStartIndex, nMiddle - 1);
+        } else {
+            return binarySearchRecursion(ArraySorted, NumberSearch, nMiddle + 1, nEndIndex);
+        }
+    } else if (nStartIndex === nEndIndex) {
+        if (NumberSearch === ArraySorted[nStartIndex]) {
+            return nStartIndex;
+        }
+        return - 1;
+    } else {
+        return - 1;
+    }
+}
 
+/////
+/////测试----------
 console.log(insertSort([3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8]));
 const ArrayMerge = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+const ArrayMerge2 = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+
+ArrayMerge2.sort((a, b) => {
+    return a - b;
+});
+console.log(`standard sort ${ArrayMerge2}`);
+const ArrayMerge3 = [3, 5, 1, 8, 1, 5, 0, 12, 1, 3, 4, 5, 7, 8];
+insertSortRecursion(ArrayMerge3, ArrayMerge3.length - 1);
+console.log(`insert sort ${ArrayMerge3}`);
+
 mergeSort(ArrayMerge, 0, ArrayMerge.length - 1);
 console.log(ArrayMerge);
+console.log('binarySearchIteration test', binarySearchIteration(ArrayMerge, 12));
+console.log('binarySearchRecursion test', binarySearchRecursion(ArrayMerge, 12, 0, ArrayMerge.length - 1));
 function testCaseInsertSort() {
-    const ArrayTest = utilityTools.generateRandomArray(1,100,20);
+    const ArrayTest = utilityTools.generateRandomArray(1, 100, 20);
     console.log(`orgin sequence is ${ArrayTest}`);
     insertSort(ArrayTest);
     console.log(`sequence sorted is ${ArrayTest}`);
