bf4272beee453a9e7558c7b8a1ef7e6794b747c9
zhengw060024 <zhengwei060024@hotmail.com>
2018-03-16 22:34:25 +0800

    算法导论习题7-4 部分代码

--- algorithemJs/chapter7/chapter7_2.js
+++ algorithemJs/chapter7/chapter7_2.js
@@ -24,19 +24,98 @@ function quickDepart(arrayInput, nStartIndex, nEndIndex) {
     }
     return [i, k];
 }
+function quickDepartTemplate(arrayInput, nStartIndex, nEndIndex, cmp) {
+    // 假设枢轴为nStartIndex
+    var i = nStartIndex, j = nStartIndex + 1, k = nStartIndex;
+    for (j = nStartIndex + 1; j <= nEndIndex; ++j) {
+        if (cmp(arrayInput[j], arrayInput[i]) < 0) {
+            var nTemp = arrayInput[i];
+            arrayInput[i] = arrayInput[j];
+            arrayInput[j] = arrayInput[++k];
+            arrayInput[k] = nTemp;
+            ++i;
+        }
+        else if (cmp(arrayInput[j], arrayInput[i]) === 0) {
+            var nTemp = arrayInput[i];
+            arrayInput[i] = arrayInput[j];
+            arrayInput[j] = arrayInput[++k];
+            arrayInput[k] = nTemp;
+        }
+    }
+    return [i, k];
+}
 var TestCase = /** @class */ (function () {
     function TestCase() {
     }
     TestCase.prototype.testDepart = function () {
-        var ItemCount = utilitytools_1.default.generateRandom(5, 20);
+        var ItemCount = utilitytools_1.default.generateRandom(5, 200);
         console.log('lenth is ', ItemCount);
         var arrayInput = utilitytools_1.default.generateRandomArray(1, 10, ItemCount);
         console.log("orgin array is " + arrayInput);
-        var nReturnTuple = quickDepart(arrayInput, 0, arrayInput.length - 1);
+        var arrayToDepart = arrayInput.concat();
+        var nReturnTuple = quickDepart(arrayToDepart, 0, arrayInput.length - 1);
         console.log("depart index is " + nReturnTuple + " ");
-        console.log("departed array is " + arrayInput);
+        console.log("departed array is " + arrayToDepart);
+        var bCheckDepartSuccess = this.checkArrayDepartSuccess(arrayToDepart, arrayInput, nReturnTuple[0], nReturnTuple[1]);
+        if (bCheckDepartSuccess) {
+            console.log('check depart is success!!!');
+        }
+        else {
+            console.log('check depart is error!!!');
+        }
+    };
+    TestCase.prototype.testDepartTemplate = function () {
+        var ItemCount = utilitytools_1.default.generateRandom(5, 200);
+        console.log('lenth is ', ItemCount);
+        var arrayInput = utilitytools_1.default.generateRandomArray(1, 10, ItemCount);
+        console.log("orgin array is " + arrayInput);
+        var arrayToDepart = arrayInput.concat();
+        var nReturnTuple = quickDepartTemplate(arrayToDepart, 0, arrayInput.length - 1, function (a, b) {
+            return a - b;
+        });
+        console.log("depart index is " + nReturnTuple + " ");
+        console.log("departed array is " + arrayToDepart);
+        var bCheckDepartSuccess = this.checkArrayDepartSuccess(arrayToDepart, arrayInput, nReturnTuple[0], nReturnTuple[1]);
+        if (bCheckDepartSuccess) {
+            console.log('check depart is success!!!');
+        }
+        else {
+            console.log('check depart is error!!!');
+        }
+    };
+    TestCase.prototype.checkArrayDepartSuccess = function (arrayInput, arrayOrgin, rangeSameStart, rangeSameEnd) {
+        if (arrayInput.length !== arrayOrgin.length) {
+            return false;
+        }
+        for (var i = 0; i < rangeSameStart; ++i) {
+            if (arrayInput[i] >= arrayInput[rangeSameStart]) {
+                return false;
+            }
+        }
+        for (var j = rangeSameEnd + 1; j < arrayInput.length; ++j) {
+            if (arrayInput[j] <= arrayInput[rangeSameEnd]) {
+                return false;
+            }
+        }
+        for (var i = rangeSameStart; i <= rangeSameEnd; ++i) {
+            if (arrayInput[i] != arrayInput[rangeSameStart]) {
+                return false;
+            }
+        }
+        var arrayInputTemp = arrayInput.sort(function (a, b) {
+            return a - b;
+        });
+        var arrayOrginTemp = arrayInput.sort(function (a, b) {
+            return a - b;
+        });
+        for (var i = 0; i < arrayInputTemp.length; ++i) {
+            if (arrayInputTemp[i] !== arrayOrginTemp[i]) {
+                return false;
+            }
+        }
+        return true;
     };
     return TestCase;
 }());
 var testCaseItem = new TestCase();
-testCaseItem.testDepart();
+testCaseItem.testDepartTemplate();
--- algorithemJs/chapter7/chapter7_2.ts
+++ algorithemJs/chapter7/chapter7_2.ts
@@ -16,26 +16,103 @@ function quickDepart(arrayInput: Array<number>, nStartIndex: number, nEndIndex: 
             arrayInput[j] = arrayInput[++k];
         }
     }
-    for(let t = i; t <= k; ++t) {
+    for (let t = i; t <= k; ++t) {
         arrayInput[t] = nTemp;
     }
-    
+
     return [i, k];
+}
+function quickDepartTemplate<T>(arrayInput: Array<T>, nStartIndex: number, nEndIndex: number,
+    cmp: (a: T, b: T) => number) {
+        // 假设枢轴为nStartIndex
+        let i = nStartIndex, j = nStartIndex + 1, k = nStartIndex;
+        for (j = nStartIndex + 1; j <= nEndIndex; ++j) {
+            if (cmp(arrayInput[j],arrayInput[i]) < 0) {
+                let nTemp = arrayInput[i];
+                arrayInput[i] = arrayInput[j];
+                arrayInput[j] = arrayInput[++k];
+                arrayInput[k] = nTemp;
+                ++i;
+            } else if(cmp(arrayInput[j],arrayInput[i]) === 0) {
+                let nTemp = arrayInput[i];
+                arrayInput[i] = arrayInput[j];
+                arrayInput[j] = arrayInput[++k];
+                arrayInput[k] = nTemp;
+            }
+        }
+        return [i, k];    
 }
 class TestCase {
     constructor() {
 
     }
     testDepart() {
-        const ItemCount = utilityTools.generateRandom(5, 20);
-        console.log('lenth is ' ,ItemCount);
+        const ItemCount = utilityTools.generateRandom(5, 200);
+        console.log('lenth is ', ItemCount);
         const arrayInput = utilityTools.generateRandomArray(1, 10, ItemCount);
         console.log(`orgin array is ${arrayInput}`);
-        let nReturnTuple = quickDepart(arrayInput, 0, arrayInput.length - 1);
+        const arrayToDepart = arrayInput.concat();
+        let nReturnTuple = quickDepart(arrayToDepart, 0, arrayInput.length - 1);
         console.log(`depart index is ${nReturnTuple} `);
-        console.log(`departed array is ${arrayInput}`);
-
+        console.log(`departed array is ${arrayToDepart}`);
+        const bCheckDepartSuccess = this.checkArrayDepartSuccess(arrayToDepart, arrayInput, nReturnTuple[0], nReturnTuple[1]);
+        if (bCheckDepartSuccess) {
+            console.log('check depart is success!!!');
+        } else {
+            console.log('check depart is error!!!')
+        }
+    }
+    testDepartTemplate() {
+        const ItemCount = utilityTools.generateRandom(5, 200);
+        console.log('lenth is ', ItemCount);
+        const arrayInput = utilityTools.generateRandomArray(1, 10, ItemCount);
+        console.log(`orgin array is ${arrayInput}`);
+        const arrayToDepart = arrayInput.concat();
+        let nReturnTuple = quickDepartTemplate<number>(arrayToDepart, 0, arrayInput.length - 1,(a:number,b:number) => {
+            return a - b;
+        });
+        console.log(`depart index is ${nReturnTuple} `);
+        console.log(`departed array is ${arrayToDepart}`);
+        const bCheckDepartSuccess = this.checkArrayDepartSuccess(arrayToDepart, arrayInput, nReturnTuple[0], nReturnTuple[1]);
+        if (bCheckDepartSuccess) {
+            console.log('check depart is success!!!');
+        } else {
+            console.log('check depart is error!!!')
+        }
+    }
+    private checkArrayDepartSuccess(arrayInput: Array<number>, arrayOrgin: Array<number>
+        , rangeSameStart: number, rangeSameEnd: number) {
+        if (arrayInput.length !== arrayOrgin.length) {
+            return false;
+        }
+        for (let i = 0; i < rangeSameStart; ++i) {
+            if (arrayInput[i] >= arrayInput[rangeSameStart]) {
+                return false;
+            }
+        }
+        for (let j = rangeSameEnd + 1; j < arrayInput.length; ++j) {
+            if (arrayInput[j] <= arrayInput[rangeSameEnd]) {
+                return false;
+            }
+        }
+        for (let i = rangeSameStart; i <= rangeSameEnd; ++i) {
+            if (arrayInput[i] != arrayInput[rangeSameStart]) {
+                return false;
+            }
+        }
+        const arrayInputTemp = arrayInput.sort((a: number, b: number) => {
+            return a - b;
+        });
+        const arrayOrginTemp = arrayInput.sort((a: number, b: number) => {
+            return a - b;
+        });
+        for (let i = 0; i < arrayInputTemp.length; ++i) {
+            if (arrayInputTemp[i] !== arrayOrginTemp[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 const testCaseItem = new TestCase();
-testCaseItem.testDepart();
+testCaseItem.testDepartTemplate();
